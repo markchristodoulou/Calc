d00370b2ab5c0edd4f16c65f9e64c9d211914ee0
Mark Christodoulou <Mark@Marks-MacBook-Pro-2.local>
2019-02-25 21:57:06 -0700

    Final part but only part with my name and class ID because I forgot to put it at first

--- CSEProject2_360/Calculator.java
+++ CSEProject2_360/Calculator.java
@@ -1,9 +1,17 @@
+/* 
+Name: Mark Christodoulou
+Class ID:349
+Description: This program contains six methods, these methods are to add, subtract, multiply,
+divide, display total, and display the history of operations.
+*/
 package CSEProject2_360;
 
 public class Calculator {
 	private int total;
+	private String history;
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0"; // initilaze the history string
 	}
 	
 	public int getTotal () // Returns the total of the Calculation
@@ -11,28 +19,33 @@ public class Calculator {
 		return total;
 	}
 	
-	public void add (int value) { //adds the value of the parameter to the total 
+	public void add (int value) { //adds the value of the parameter to the total and updates history
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
-	public void subtract (int value) {//subtracts the value of the parameter to the total 
+	public void subtract (int value) {//subtracts the value of the parameter to the total and updates history
 		total = total - value;
+		history = history + " – " + value;
 	}
 	
-	public void multiply (int value) {//multiplies the value of the parameter to the total 
+	public void multiply (int value) {//multiplies the value of the parameter to the total and updates history
 		total = total * value;
+		history = history + " * " + value;
 	}
 	
-	public void divide (int value) {//divides the value of the parameter to the total 
+	public void divide (int value) {//divides the value of the parameter to the total and updates history
 		if(value == 0){
 			total = 0;
+			history = history + " / 0";
 		}
 		else{
 			total = total / value;
+			history = history + " / " + value;
 		}
 	}
 	
-	public String getHistory () {
-		return "";
+	public String getHistory () {// returns the history
+		return history;
 	}
 }
