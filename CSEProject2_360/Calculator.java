3b5315585a42714f3d6ec279dfd7f630c4a25e45
Mark Christodoulou <Mark@Marks-MacBook-Pro-2.local>
2019-02-25 21:21:58 -0700

    Second commit

--- CSEProject2_360/Calculator.java
+++ CSEProject2_360/Calculator.java
@@ -7,24 +7,25 @@ public class Calculator {
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal () // Returns the total of the Calculation
+	{
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	public void add (int value) { //adds the value of the parameter to the total 
+		total = total + value;
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) {//subtracts the value of the parameter to the total 
+		total = total - value;
 	}
 	
-	public void multiply (int value) {
-		
+	public void multiply (int value) {//multiplies the value of the parameter to the total 
+		total = total * value;
 	}
 	
-	public void divide (int value) {
-		
+	public void divide (int value) {//divides the value of the parameter to the total 
+		total = total / value;
 	}
 	
 	public String getHistory () {
